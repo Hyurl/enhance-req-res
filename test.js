a495d84d277e360543e2e100f4cf23f79dc651c4
hyurl <i@hyurl.com>
2017-12-15 20:34:12 +0800

    First commit.

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ http.createServer((req, res) => {
     res.headers["x-powered-by"] = "Ayonium";
     res.lastModified = new Date(1970);
     res.cookies.user = req.auth ? req.auth.username : "anonymous";
-    req.status = 200;
+
     res.send(["Hello", "World"]);
 
     console.log("Cost time: %d ms", Date.now() - req.time);
